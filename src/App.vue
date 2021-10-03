4d6cc1460a669b9bfd6a7db57c8e66a464b5f314
haktantekin <47484364+haktantekin@users.noreply.github.com>
2021-10-03 13:20:03 +0300

    Learn Input with VueJS

---
+++ src/App.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="container">
+    <h3>Form Verileriyle Çalışmak</h3>
+    <hr>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="panel panel-warning">
+          <div class="panel-heading">
+            <h4>Başvuru Formu</h4>
+          </div>
+          <div class="panel-body">
+            <form>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <label for="email">Kullanıcı Adı</label>
+                    <input type="text" id="username" class="form-control" :value="userData.username" @input="userData.username = $event.target.value">
+                  </div>
+                  <div class="form-group">
+                    <label for="password">Şifre</label>
+                    <input type="password" id="password" class="form-control" v-model.lazy="userData.password">
+                  </div>
+                  <div class="form-group">
+                    <label for="age">Yaş</label>
+                    <input type="number" id="age" class="form-control" v-model.number.trim="userData.age">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <label for="message">Açıklama</label><br>
+                  <textarea id="message" rows="3" class="form-control" v-model="userData.mesage"></textarea>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <label>
+                      <input v-model="userData.interests" type="checkbox" value="yazilim"> Yazılım
+                    </label>
+                    <label>
+                      <input v-model="userData.interests" type="checkbox" value="donanim"> Donanım
+                    </label>
+                  </div>
+
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <label>
+                    <input v-model="userData.gender" type="radio" value="erkek"> Erkek
+                  </label>
+                  <label>
+                    <input v-model="userData.gender" type="radio" value="kadin"> Kadın
+                  </label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 from-group">
+                  <label>Şehir</label>
+                  <select class="form-control" v-model="userData.selectedCities">
+                    <option :selected="city == 'Adana'" v-for="city in userData.cities">{{ city }}</option>
+                  </select>
+                </div>
+              </div>
+              <hr>
+               <div class="row">
+                <div class="col-md-12 form-group">
+                  <app-switch v-model="switched"></app-switch>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <button
+                    @click.prevent="submit"
+                    class="btn btn-primary">Gönder!
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6" v-if="isSubmitted">
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h4>Form Verileri</h4>
+            </div>
+            <div class="panel-body">
+              <p>Kullanıcı Adı: {{ userData.username }}</p>
+              <p>Şifre: {{ userData.password }}</p>
+              <p>Yaş: {{ userData.age }}</p>
+              <p style="white-space:pre;">Açıklama: {{ userData.mesage }}</p>
+              <p><strong>İlgi Alanları</strong></p>
+              <ul>
+                <li v-for="item in userData.interests">{{ item }}</li>
+              </ul>
+              <p>Cinsiyet: {{ userData.gender }}</p>
+              <p>Şehir: {{ userData.selectedCities }}</p>
+              <p>Toggle:  {{ switched }}</p>
+            </div>
+          </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Switch from "./Switch"
+
+  export default {
+    components:{
+      appSwitch: Switch
+    },
+    data(){
+      return{
+        userData : {
+          username:'',
+          password:'',
+          age: 28,
+          message:'',
+          interests: [],
+          gender:'',
+          cities:["İstanbul", "Ankara", "İzmir", "Adana","Bursa","Konya","Trabzon"],
+          selectedCities:''
+        },
+        switched:true,
+        isSubmitted:false
+        
+      }
+    },
+    methods:{
+      submit(){
+        this.isSubmitted=true
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
